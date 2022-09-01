57fdc06b3f4b546df9727541c7ad085ec0d41a71
mei <mei28aquarius@gmail.com>
2022-09-01 14:19:36 +0900

    add

--- exp/20220609/utils/func.py
+++ exp/20220609/utils/func.py
@@ -1,9 +1,11 @@
 import multiprocessing
+from fastprogress import progress_bar
 
 processes = multiprocessing.cpu_count()
 
 
-def multiprocess_imap(func, args, processes: int = processes):
+def multiprocess_imap(func, args, processes: int = processes, verbose=False):
     with multiprocessing.Pool(processes=processes) as pool:
-        for _ in pool.imap(func, args):
-            pass
+        imap = pool.imap(func, args)
+        if verbose:
+            progress = list(progress_bar(imap, total=len(args)))
